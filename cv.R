77f2f892d559d14d773c595095c19395cb09eafb
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-11-29 15:20:03 -0600

    perform_cv seems to work well for glmnet.

--- cv.R
+++ cv.R
@@ -1,18 +1,48 @@
 # TO DO: Explain briefly what this function does, and how to use it.
-perform_cv <- function (fit, evaluate, dat, cvpar, noncvpar = NULL, k = 5) {
+perform_cv <- function (fit, predict, evaluate, dat, cvpar, noncvpar = NULL,
+                        k = 5) {
 
   # Get the number of data samples.
   n <- nrow(dat)
 
+  # Get the number of parameter settings.
+  m <- length(cvpar)
+  
+  # Assign the data samples (rows of dat) into k groups ("folds").
+  folds <- sample(rep(1:k,length.out = n))
+
+  # Set up the cross-validation matrix.
+  out <- matrix(0,m,k)
+  if (is.atomic(cvpar))
+    rownames(out) <- cvpar
+  colnames(out) <- paste0("k",1:k)
+  
   # Convert cvpar to a list if necessary.
   if (!is.list(cvpar))
     cvpar <- as.list(cvpar)
   
-  # Assign the data samples (rows of dat) into k groups ("folds").
-  folds <- sample(n,k)
+  # Repeat for each fold.
+  for (j in 1:k) {
 
-  browser()
+    # Construct the training and test sets.
+    train <- dat[folds != j,]
+    test  <- dat[folds == j,]
+
+    # Repeat for each parameter setting.
+    for (i in 1:length(cvpar)) {
+
+      # Fit the model to the training data.
+      model <- fit(train,cvpar[[i]],noncvpar)
+
+      # Make predictions in the test samples.
+      pred <- predict(test,model)
+
+      # Evaluate the predictions, and store the error measure in the
+      # cross-validation matrix.
+      out[i,j] <- evaluate(pred,test)
+    }
+  }
   
-  # Return ...
-  return(list())
+  # Return the cross-validation matrix.
+  return(out)
 }
--- elastic_net_demo.R
+++ elastic_net_demo.R
@@ -14,19 +14,28 @@ b[sample(p,4)] <- c(1,-1,1,-1)
 y <- drop(X %*% b + rnorm(n))
 
 # Fit an Elastic Net model using glmnet.
-fit_glmnet <- function (dat, cvpar)
-  glmnet(X = dat[,-1],y = dat[,1],lambda = cvpar,alpha = 0.5)
+fit_glmnet <- function (dat, cvpar, noncvpar)
+  glmnet(x = dat[,-1],y = dat[,1],lambda = cvpar,alpha = 0.5)
 
-# Compute the root mean squared error.
-rmse <- function (pred, true) {
-
+# Predict Y using the fitted Elastic Net model.
+predict_glmnet <- function (dat, model) {
+  y <- predict(model,dat[,-1])
+  dat[,1] <- y
+  return(dat)
 }
+
+# Compute the mean squared error (MSE) between the predicted Y and
+# true Y.
+compute_mse <- function (pred, true)
+  mean((pred[,1] - true[,1])^2)
 
 # Perform cross-validation to choose the penalty strength of the
 # Elastic Net model.
-lambda <- rev(exp(seq(-4.5,0.2,length.out = 100)))
-res <- perform_cv(fit_glmnet,compute_rmse,cbind(y,X),lambda)
+lambda <- round(rev(exp(seq(-3.75,0.85,length.out = 100))),digits = 4)
+cv <- perform_cv(fit_glmnet,predict_glmnet,compute_mse,cbind(y,X),lambda)
 
 # Compare with cv.glmnet.
-# TO DO.
-
+res <- cv.glmnet(X,y,alpha = 0.5)
+plot(res$lambda,res$cvm,type = "l",col = "darkblue",lwd = 2,log = "x",
+     xlab = "lambda",ylab = "mse")
+lines(lambda,rowMeans(cv),col = "darkorange",lwd = 2,lty = "dashed")
