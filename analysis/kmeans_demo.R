1178ef4c84feb76fcde47073fed0cd8902d59347
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-12-07 19:38:30 -0600

    Working on kmeans demo.

--- analysis/kmeans_demo.R
+++ analysis/kmeans_demo.R
@@ -1,3 +1,28 @@
-# TO DO: Give an overview of this demo.
-library(glmnet)
-source("cv.R")
+source("../code/cv.R")
+set.seed(1)
+
+# Simulate a clustering data set.
+n <- 200
+k <- 5
+centers <- matrix(rnorm(2*k),k,2)
+membership <- sample(k,n,replace = TRUE)
+X <- matrix(0,n,2)
+for (i in 1:n) {
+  j     <- membership[i]
+  X[i,] <- centers[j,] + rnorm(2)/3
+}
+plot(X[,1],X[,2],col = "royalblue",pch = 1,cex = 0.75,xlab = "x1",ylab = "x2")
+points(centers[,1],centers[,2],col = "red",pch = 20,cex = 1)
+
+# This function runs k-means.
+run_kmeans <- function (x, y, cvpar, noncvpar, init)
+  kmeans(x,init[1:cvpar,],iter.max = 100)
+
+# Choose the "best-fit" cluster centers for the data points x.
+predict_kmeans <- function (x, model)
+  predict(model,x)
+
+# This function computes the mean squared error (MSE) between the
+# estimated cluster centers and the true cluster centers.
+compute_mse <- function (pred, true)
+  mean((pred - true)^2)
